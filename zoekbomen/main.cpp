caab933ee7379a7ea4cf562eed027fc6ed4d143e
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2014-10-01 11:32:20 +0200

    added files of week 2

---
+++ zoekbomen/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <ios>
+
+#include "zoekboom.h"
+
+using namespace std;
+using std::pair;
+using std::cout;
+using std::endl;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+	Zoekboom<int,int> boom;
+
+	boom.voegtoe(1,2);
+	boom.voegtoe(2,3);
+
+    // for(int i = 0; i < 100; i++) {
+    // 	boom.voegtoe(i,i);
+    // }
+
+    cout << boom << endl;
+
+    return 0;
+}
