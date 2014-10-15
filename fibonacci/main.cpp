7013eb946c75a630677cb0040be5144a49b07773
unknown <Maarten Van Dessel@weierstrass.iii.hogent.be>
2014-10-15 11:45:17 +0200

    main.cpp: - maak gebruik van deque en pairs

--- fibonacci/main.cpp
+++ fibonacci/main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+	using std::cout;
+	using std::endl;
 #include <fstream>
+	using std::ifstream;
 #include <sstream>
+	using std::istringstream;
 #include <vector>
+	using std::vector;
 #include <string>
-
-using namespace std;
+	using std::string;
+#include "fibonacci.h"
 
 int main(int argc, char ** argv) {
-	int k, temp;
-	vector<int> a_waarden;
-	vector<int> start_waarden;
-	vector<int> n_waarden;
+	int k;
+	vector<pair<int, int> > a_waarden;	// <index, a-waarde>
+	deque<int> start_waarden;
+	vector<pair<int, int> > n_waarden;	// <n-waarde, f(n)>
 	
 	ifstream input("input.txt");
 	if(input.good()) {
 		string line;
 		istringstream ss;
+		int temp;
 		// k inlezen
 		getline(input, line);
 		ss.clear();
@@ -25,8 +31,13 @@ int main(int argc, char ** argv) {
 		getline(input, line);
 		ss.clear();
 		ss.str(line);
+		int i = 0;	// index van a
 		while(ss >> temp) {
-			a_waarden.push_back(temp);
+			// enkel nuttige coefficienten opslaan (a != 0)
+			if(temp != 0) {
+				a_waarden.push_back(make_pair(i, temp));
+			}
+			i++;
 		}
 		// start-waarden inlezen
 		getline(input, line);
@@ -37,14 +48,14 @@ int main(int argc, char ** argv) {
 		}
 		// n-waarden inlezen
 		while(input >> temp) {
-			n_waarden.push_back(temp);
+			n_waarden.push_back(make_pair(temp, 0));
 		}
 	}
 	
 	cout << "k = " << k << endl;
 	cout << "a-waarden:" << endl;
 	for(int i=0; i<a_waarden.size(); i++) {
-		cout << "\t- " << a_waarden.at(i) << endl;
+		cout << "\t[" << a_waarden.at(i).first << "]\t=> " << a_waarden.at(i).second << endl;
 	}
 	cout << "start-waarden:" << endl;
 	for(int i=0; i<start_waarden.size(); i++) {
@@ -52,9 +63,10 @@ int main(int argc, char ** argv) {
 	}
 	cout << "n-waarden:" << endl;
 	for(int i=0; i<n_waarden.size(); i++) {
-		cout << "\t- " << n_waarden.at(i) << endl;
+		cout << "\t- " << n_waarden.at(i).first << endl;
 	}
 	
+	Fibonacci(start_waarden, a_waarden);
 	
 	return 0;	
 }
